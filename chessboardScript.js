d0fee4c5ea0640ff8e97de9bef95c2e16a3aea69
MasterJCoz <masterjcoz@gmail.com>
2024-12-19 12:10:05 +0000

    bugfix

--- chessboardScript.js
+++ chessboardScript.js
@@ -54,7 +54,7 @@ const pileName_Main = "MainPile";
 const pileName_White = "WhitePile";
 const pileName_Black = "BlackPile";
 let FindingData = false;
-const delayChecks = 800;
+const delayChecks = 1400;
 
 
 let turnturn = 1;
@@ -905,7 +905,9 @@ async function SendData(x1, y1, x2, y2, img_ = null, startIgnore = false) {
 
     
     //pull cards - for api process
-    d = await GetDataFromApi(`https://deckofcardsapi.com/api/deck/${gameId}/draw/?count=52`);
+    // d = await GetDataFromApi(`https://deckofcardsapi.com/api/deck/${gameId}/draw/?count=52`);
+    d = await GetDataFromApi(`https://deckofcardsapi.com/api/deck/${gameId}/pile/${pileName_Main}/draw/?cards=AS,2S,3S,4S,5S,6S,7S,8S,9S,0S,JS,QS,KS,AD,2D,3D,4D,5D,6D,7D,8D,9D,0D,JD,QD,KD,AC,2C,3C,4C,5C,6C,7C,8C,9C,0C,JC,QC,KC,AH,2H,3H,4H,5H,6H,7H,8H,9H,0H,JH,QH,KH`);
+
 
 
     let pilepile = pileName_White;
@@ -930,7 +932,8 @@ async function SendData(x1, y1, x2, y2, img_ = null, startIgnore = false) {
     }
 
     console.log(`data sent - ${xx1} ${yy1} ${xx2} ${yy2}`)
-    WaitingForEntry(pilepile);
+    
+    await WaitingForEntry(pilepile);
 
     
 
@@ -947,7 +950,7 @@ async function WaitingForEntry(pilepile_) {
     while (FindingData == true & (attemptsWhileLoop < 1000) ) {
 
 
-        console.log(`https://deckofcardsapi.com/api/deck/${gameId}/pile/${pilepile_}/list/`)
+        // console.log(`https://deckofcardsapi.com/api/deck/${gameId}/pile/${pilepile_}/list/`)
 
 
         // this is checking for other players data
@@ -960,7 +963,8 @@ async function WaitingForEntry(pilepile_) {
                 c = d?.piles?.BlackPile?.remaining ?? -1;    
             }
             if (c > 0) {   
-                ReadData(d, pilepile_);
+                await ReadData(d, pilepile_);
+                FindingData = false;
                 break;
             }
         }
@@ -1106,7 +1110,6 @@ async function ReadData(dd, pile__) {
 
 
         //clear api stuff     
-
         d = await GetDataFromApi(`https://deckofcardsapi.com/api/deck/${gameId}/pile/${pile__}/draw/?cards=${dd[0].code},${dd[1].code},${dd[2].code},${dd[3].code}`);
         // Push back into pile
         d = await GetDataFromApi(`https://deckofcardsapi.com/api/deck/${gameId}/pile/${pileName_Main}/add/?cards=AS,2S,3S,4S,5S,6S,7S,8S,9S,0S,JS,QS,KS,AD,2D,3D,4D,5D,6D,7D,8D,9D,0D,JD,QD,KD,AC,2C,3C,4C,5C,6C,7C,8C,9C,0C,JC,QC,KC,AH,2H,3H,4H,5H,6H,7H,8H,9H,0H,JH,QH,KH`);
